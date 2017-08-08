571abfef7825a0ea6b6a61ad6b5d1e2738598122
ctanes <ctanes@gmail.com>
2017-08-08 01:01:14 -0400

    Aded tests for is_same_run and additional properties

--- test/test_illumina.py
+++ test/test_illumina.py
@@ -13,6 +13,7 @@ class IlluminaTests(unittest.TestCase):
         fastq_filepath = (
             "Miseq/160511_M03543_0047_000000000-APE6Y/Data/Intensities/"
             "BaseCalls/Undetermined_S0_L001_R1_001.fastq.gz")
+        folder_info = {"date":"160511", "instrument":"M03543", "run_number":"47", "flowcell_id":"000000000-APE6Y", "lane":"1", "read_or_index":"R", "read":"1"}
         fastq_file.name = fastq_filepath
         fq = IlluminaFastq(fastq_file)
 
@@ -21,6 +22,8 @@ class IlluminaTests(unittest.TestCase):
         self.assertEqual(fq.lane, "1")
         self.assertEqual(fq.filepath, fastq_filepath)
         self.assertEqual(fq.run_name, "160511_M03543_0047_000000000-APE6Y")
+
+        self.assertDictEqual(fq.folder_info, folder_info)
 
     def test_fp_vs_content(self):
         # check correct case for Miseq data
@@ -132,3 +135,25 @@ class IlluminaTests(unittest.TestCase):
         fq = IlluminaFastq(gzip.GzipFile(fastq_filepath))
         self.assertTrue(fq.check_file_size(50))
         self.assertFalse(fq.check_file_size(50000))
+
+    def test_is_same_run(self):
+        fastq_file = StringIO(
+            u"@M03543:47:C8LJ2ANXX:1:2209:1084:2044 1:N:0:NNNNNNNN+NNNNNNNN")
+        fastq_filepath = (
+            "Miseq/160511_M03543_0047_000000000-APE6Y/Data/Intensities/"
+            "BaseCalls/Undetermined_S0_L001_R1_001.fastq.gz")
+        fastq_file.name = fastq_filepath
+        fq = IlluminaFastq(fastq_file)
+        fastq_file.seek(0)
+        fq1 = IlluminaFastq(fastq_file)
+
+        fastq_file = StringIO(
+            u"@D00727:27:CA7HHANXX:1:1105:1243:1992 1:N:0:NGATCAGT+NNAAGGAG")
+        fastq_filepath = (
+            "Hiseq/170330_D00727_0027_ACA7HHANXX/Data/Intensities/"
+            "BaseCalls/Undetermined_S0_L001_R1_001.fastq.gz")
+        fastq_file.name = fastq_filepath
+        fq2 = IlluminaFastq(fastq_file)
+
+        self.assertTrue(fq.is_same_run(fq1))
+        self.assertFalse(fq.is_same_run(fq2))
